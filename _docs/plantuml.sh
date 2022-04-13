8c0b758eb5c782254939ec38128e64b394bb7d57
implex1v <implex1v@gmail.com>
2022-04-13 20:52:19 +0200

    (_docs) try to fix plantuml workflow, #10

--- _docs/plantuml.sh
+++ _docs/plantuml.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -exu
+cd "$(dirname "$0")"
+
+if [[ ! -f plantuml.jar ]]; then
+    wget https://github.com/plantuml/plantuml/releases/download/v1.2022.4/plantuml-1.2022.4.jar -O ./plantuml.jar
+fi
+
+echo pwd
 DIRS=$(find . -type d | paste -s -d" " -)
-plantuml -r -v $DIRS
+java -jar plantuml.jar "**/*.puml"
 
