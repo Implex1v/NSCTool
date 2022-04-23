042444bb6bd897ab1bb7e4eb1eb6ca3613fad528
implex1v <implex1v@gmail.com>
2022-04-23 21:06:27 +0200

    (api) Remove flywaydb for now, #13

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	//implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.flywaydb:flyway-core")
+	//implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
