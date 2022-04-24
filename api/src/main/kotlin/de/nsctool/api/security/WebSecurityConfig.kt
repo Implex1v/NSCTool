9a6319db24fee2cc790747911ff81720a1f6c572
implex1v <implex1v@gmail.com>
2022-04-24 10:57:56 +0200

    (api) Add spring security and configuration, #13

---
+++ api/src/main/kotlin/de/nsctool/api/security/WebSecurityConfig.kt
@@ -0,0 +1,16 @@
+package de.nsctool.api.security
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class WebSecurityConfig: WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity?) {
+        http ?: throw IllegalStateException("http security is null")
+
+        http.authorizeHttpRequests()
+            .antMatchers("/health").permitAll()
+            .anyRequest().authenticated()
+    }
+}
