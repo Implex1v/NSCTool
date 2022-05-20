e36389a360a6facb07fbf2c8dfc1e7841f95ee9c
implex1v <implex1v@gmail.com>
2022-05-20 18:37:05 +0200

    (api) Simplify user model, #13

--- api/src/main/kotlin/de/nsctool/api/model/User.kt
+++ api/src/main/kotlin/de/nsctool/api/model/User.kt
@@ -1,18 +1,17 @@
 package de.nsctool.api.model
 
 import org.hibernate.annotations.GenericGenerator
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToMany
 import javax.persistence.Table
 
 @Entity
 @Table(name = "users")
-class User: UserDetails {
+class User {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
@@ -24,18 +23,9 @@ class User: UserDetails {
     @Column(unique = true)
     var userName: String = ""
 
-    var pwd: String = ""
-
     @Column(unique = true)
     var email: String = ""
 
-    var enabled: Boolean = true
-
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
-    override fun getPassword(): String = pwd
-    override fun getUsername(): String = userName
-    override fun isAccountNonExpired(): Boolean = true
-    override fun isAccountNonLocked(): Boolean = true
-    override fun isCredentialsNonExpired(): Boolean = true
-    override fun isEnabled(): Boolean = enabled
+    @OneToMany()
+    var characters: List<Character> = emptyList()
 }
