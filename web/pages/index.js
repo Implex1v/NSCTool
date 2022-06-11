309497c5b17b8ec88f77f2db44d6d14ed08446e9
Implex1v <implex1v@gmail.com>
2022-06-11 10:24:30 +0200

    (.github) Add npm build to api-client/ts

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
-import { getSortedPostsData } from '../lib/posts'
 
 export default function Home() {
   return (
