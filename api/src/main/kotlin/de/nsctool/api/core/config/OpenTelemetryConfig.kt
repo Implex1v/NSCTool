faa8dcfcc3f2bea6380a60c2f2f217979cb3718a
Implex1v <Implex1v@gmail.com>
2023-01-20 17:34:13 +0100

    (api) Configure tracing

--- api/src/main/kotlin/de/nsctool/api/core/config/OpenTelemetryConfig.kt
+++ api/src/main/kotlin/de/nsctool/api/core/config/OpenTelemetryConfig.kt
@@ -1,14 +1,18 @@
 package de.nsctool.api.core.config
 
-import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class OpenTelemetryConfig {
+    @Value("\${tracing.host}")
+    private lateinit var otlpHost: String
+
     @Bean
-    fun exporter(): ZipkinSpanExporter = ZipkinSpanExporter
+    fun exporter(): OtlpHttpSpanExporter = OtlpHttpSpanExporter
         .builder()
-        .setEndpoint("http://tempo.observability.svc.cluster.local:9411")
+        .setEndpoint("$otlpHost/v1/traces")
         .build()
 }
