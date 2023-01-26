cca30541af0927513e7f45c578bc7fc9536d02e6
Implex1v <Implex1v@gmail.com>
2023-01-26 17:49:37 +0100

    (api) Use system properties instead of evn vars

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://tempo.observability.svc.cluster.local:4318/v1/traces
 env
-OTEL_EXPORTER_OTLP_ENDPOINT=$OTEL_EXPORTER_OTLP_ENDPOINT OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://tempo.observability.svc.cluster.local:4318/v1/traces java -javaagent:/app/otelagent.jar -jar /app/api.jar
+
+java -javaagent:/app/otelagent.jar \
+  -Dotel.exporter.otlp.endpoint=$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT \
+  -Dotel.exporter.otlp.traces.endpoint=$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT \
+  -jar /app/api.jar
